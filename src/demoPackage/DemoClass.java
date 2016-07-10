040e6cb45afaf71592600e70ab95989f7ca56d1d
coorgchandan <chandan.vviet@live.com>
2016-07-10 14:26:07 +0530

    Update DemoClass.java
    fgfg

--- src/demoPackage/DemoClass.java
+++ src/demoPackage/DemoClass.java
@@ -5,6 +5,7 @@ public class DemoClass {
 	public static void main(String[] args) {
 		System.out.println("new sunday workspace");
 		System.out.println("new sunday workspace");
+		System.out.println("new sunday workspace");
 		
 
 	}
